5fdf1b34910c2256e7289a300e7d26df7b52da8a
Erik Grundy <Erik.Grundy@alstefgroup.com>
2026-01-08 10:27:53 +1300

    day 2 - messy, but it works

--- src/day6.rs
+++ src/day6.rs
@@ -35,7 +35,44 @@ fn make_initial_state(s: &str) -> (usize, fn(usize, usize) -> usize) {
 }
 
 pub fn part2(path: impl AsRef<Path>) -> usize {
-    todo!()
+    let reader = BufReader::new(File::open(path).unwrap());
+    let chars: Vec<Vec<char>> = reader
+        .lines()
+        .map(|l| l.unwrap().chars().collect())
+        .collect();
+    let columns = chars[0].len();
+    let rows = chars.len();
+    let mut state: Option<(fn(usize, usize) -> usize, usize)> = None;
+    let add = |a, b| a + b;
+    let mul = |a, b| a * b;
+    let mut grand_total = 0;
+    for c in 0..columns {
+        let number_string: String = (&chars[..rows - 1]).iter().map(|r| r[c]).collect();
+        let trimmed = number_string.trim();
+        if trimmed.is_empty() {
+            if let Some((_, acc)) = state {
+                grand_total += acc;
+            }
+            state = None;
+            continue;
+        }
+        let number: usize = trimmed.parse().unwrap();
+        state = match chars[rows - 1][c] {
+            '+' => Some((add, 0)),
+            '*' => Some((mul, 1)),
+            _ => state,
+        };
+        match state {
+            None => panic!("whoops! there's a bug here"),
+            Some((op, acc)) => {
+                state = Some((op, op(acc, number)));
+            }
+        }
+    }
+    if let Some((_, acc)) = state {
+        grand_total += acc;
+    }
+    grand_total
 }
 
 #[cfg(test)]
@@ -50,5 +87,15 @@ mod tests {
     #[test]
     fn part1_real() {
         assert_eq!(4405895212738, part1("inputs/real/6.txt"));
+    }
+
+    #[test]
+    fn part2_practice() {
+        assert_eq!(3263827, part2("inputs/practice/6.txt"));
+    }
+
+    #[test]
+    fn part2_real() {
+        assert_eq!(7450962489289, part2("inputs/real/6.txt"));
     }
 }
